1887241b504594db47791ff7629327ffb9dbc108
Kanika Sharma <kanikasharma9006@gmail.com>
2025-09-10 19:23:38 +0530

    2nd question on switch case

---
+++ Ifelse/switchcase2.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+switch(n)
+{
+    case 1:
+    cout<<"january";
+    break;
+    case 2:
+    cout<<"febuary";
+    break;
+    case 3:
+    cout<<"march";
+    break;
+    case 4:
+    cout<<"april";
+    break;
+    case 5:
+    cout<<"may ";
+    break;
+    case 6:
+    cout<<"june ";
+    break;
+    case 7:
+    cout<<"july";
+    break;
+    case 8:
+    cout<<"AUG";
+    break;
+    case 9:
+    cout<<"SEPT";
+    break;
+    case 10:
+    cout<<"OCT";
+    break;
+    case 11:
+    cout<<"NOV";
+    break;
+    case 12:
+    cout<<"DEC";
+    break;
+}
+
+
+
+    return 0;
+}
