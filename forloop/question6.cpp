56ee8c8b759eead665a249831dfd4649cecbf6bd
Kanika Sharma <kanikasharma9006@gmail.com>
2025-09-12 19:53:54 +0530

    print all alphabet using for loop

---
+++ forloop/question6.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+// write a program to print all alphabet from a to z.
+for(char i='A';i<='Z';i++)
+{
+    cout<<i<<endl;
+}
+
+
+
+    return 0;
+}
