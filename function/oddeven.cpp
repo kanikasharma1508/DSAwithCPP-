54c11f9857c0210995d608a27aed35f996c9139b
Kanika Sharma <kanikasharma9006@gmail.com>
2025-09-26 19:18:18 +0530

    check number is odd or even

---
+++ function/oddeven.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int check(int a)
+{
+ if(a%2==0)
+ {
+    return true;
+ }
+ else{
+    return false;
+ }
+}
+int main()
+{  
+int a;
+cin>>a;
+cout<<check(a);
+
+    return 0;
+}
