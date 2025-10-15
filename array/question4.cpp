1a59f6ecf9db0b001db332f1358f79a5cf008bde
Kanika Sharma <kanikasharma9006@gmail.com>
2025-10-15 23:30:26 +0530

    sum of array using function

---
+++ array/question4.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int arr[],int n)
+{
+    for(int i=0;i<=n-1;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            cout<<arr[i]<<endl;
+        }
+    }
+}
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<=n-1;i++)
+{
+    cin>>arr[i];
+}
+print(arr,n);
+
+
+    return 0;
+}
