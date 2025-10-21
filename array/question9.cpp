26cb784eec99bf33a7610f92105728bea6209ddc
Kanika Sharma <kanikasharma9006@gmail.com>
2025-10-21 19:16:25 +0530

    string cout

---
+++ array/question9.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+string student[n];
+for(int i=0;i<n;i++)
+{
+    cin>>student[i];
+}
+for(int i=0;i<n;i++)
+{
+    cout<<student[i]<<endl;
+}
+
+    return 0;
+}
