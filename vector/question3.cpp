437a7908f231cd62cf8e3c13562978dccfac72cf
Kanika Sharma <kanikasharma9006@gmail.com>
2025-11-10 19:42:32 +0530

    vectorbusing set function

---
+++ vector/question3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+set <int> s;
+for(int i=0;i<n;i++)
+{
+    s.insert(arr[i]);
+}
+for(int val : s)
+{
+    cout<<val<<" ";
+}
+
+
+    return 0;
+}
