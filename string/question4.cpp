c9edaa72464d09c84ab05b97277ed03f4209ef99
Kanika Sharma <kanikasharma9006@gmail.com>
2025-11-23 19:52:31 +0530

    convert  string into upper case

---
+++ string/question4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+string s;
+getline(cin,s);
+int count =0;
+while(s[count]!='\0')  // "/0" means null character.
+{
+  count++;
+}
+cout<<count;
+
+
+
+    return 0;
+}
