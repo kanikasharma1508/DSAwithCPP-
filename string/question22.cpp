5259d6785364eaaa225951e0c3816eb8a4886e16
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-06 20:27:08 +0530

    find beauty of string using function

---
+++ string/question22.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int beauty(string s)
+{
+    map<char,int>mpp;
+    for(int i=0;i<s.length();i++)
+    {
+        mpp[s[i]]++;
+    }
+    int min=INT_MAX;
+    int max=INT_MIN;
+    for(auto it:mpp)
+    {
+        if(it.second<min)
+        { min=it.second;
+        
+        }
+        if(it.second>max)
+        {
+            max=it.second;
+        }
+    }
+    return max-min;
+}
+int main()
+{
+string s;
+cin>>s;
+cout<<beauty(s);
+
+
+    return 0;
+}
