2b3c2aff6d98ef7f553dfcad86e0343ac30c0b12
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-18 21:48:39 +0530

    code for  member function

---
+++ OOPS/question2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class teacher{
+public:
+ void greet()
+ {
+    cout<<"good morning students";
+ }
+};
+int main()
+{
+teacher t1;
+t1.greet();
+
+
+
+    return 0;
+}
