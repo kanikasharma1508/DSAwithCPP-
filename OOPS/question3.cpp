43ab4d9fa1bc0fd586b5788d33866fce3b3b546a
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-18 22:02:32 +0530

    code for member function with data member

---
+++ OOPS/question3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class teacher{
+public:
+string name;
+void greet()
+{
+     cout<<"good morning "<<name;
+}
+};
+int main()
+{
+teacher t1;
+t1.name="harshit";
+t1.greet();
+  
+    return 0;
+}
