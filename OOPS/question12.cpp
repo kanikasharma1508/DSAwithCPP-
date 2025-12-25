83fd30a23f5b4c1d0e438e2b04718f1808c81b1c
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-26 00:02:21 +0530

    aaccess modifier(public)

---
+++ OOPS/question12.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+    public:
+    string names;
+
+};
+int main()
+{
+student s1;
+s1.names="kanika";
+cout<<s1.names;
+
+
+    return 0;
+}
