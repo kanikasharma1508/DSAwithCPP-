6c10ad7701fe840d57cfea4f665daaac2405c08f
Kanika Sharma <kanikasharma9006@gmail.com>
2026-06-22 23:29:49 +0530

    press 0 to exit game

---
+++ DSA SHEET/0exitgame.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+// digit counting game.
+int s=0;
+
+while(true)
+{
+    int n;
+    cin>>n;
+    if(n==0)
+    {
+        break;
+    }
+    s=s+n; 
+    cout<<s<<endl; 
+}
+
+    return 0;
+}
