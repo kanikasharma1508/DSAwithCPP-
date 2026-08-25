7be37d45e036cd849e3e75c2f21ce0b00e65661f
Kanika Sharma <kanikasharma9006@gmail.com>
2026-08-26 00:43:56 +0530

    bool datatype

---
+++ function/practice.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sum(int a,int b)
+{
+    int add;
+    add=a+b;
+   cout<<add;
+
+}
+int main()
+{
+int x,y;
+cin>>x>>y;
+sum(x,y);
+    return 0;
+}
---
+++ function/return.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a,int b)
+{
+    int add=a+b;
+    return add;
+}
+int main()
+{
+int a,b;
+cin>>a>>b;
+cout<<sum(a,b);
+
+
+    return 0;
+}
