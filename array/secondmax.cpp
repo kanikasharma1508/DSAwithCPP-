95652adab988ba735dc63c7fb48a19b947e70142
Kanika Sharma <kanikasharma9006@gmail.com>
2026-09-08 23:53:51 +0530

    second maximum element in array

---
+++ array/secondmax.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+int max=0;
+for(int i=0;i<n;i++)
+{
+    if(arr[i]>=max)
+    max=arr[i];
+}
+int smax=INT_MIN;
+for(int i=0;i<n;i++)
+{
+    if(arr[i]>smax && arr[i]<max)
+    smax=arr[i];
+}
+cout<<smax;
+
+    return 0;
+}
